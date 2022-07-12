496b412e63670c1c2b537816efb1ef8236bc921c
Osvaldo Júnior <osvaldojunior95@gmail.com>
2022-07-12 15:28:09 -0300

    fix: resolvido problema do handlerError

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
-import express, { json, Request, Response } from "express";
+import express from "express";
+import "express-async-errors";
 import cors from "cors";
 import router from "./routes/routes.js";
 import handlerError from "./middlewares/handlerError.js";
 import "./config.js";
-import "express-async-errors";
 
 const app = express();
 
-app.use(json());
+app.use(express.json());
 app.use(cors());
 app.use(router);
 app.use(handlerError);
 
-app.get("/", (req: Request, res: Response) => {
-  return res.send(200);
+app.listen(process.env.PORT || 4000, () => {
+    console.log("Server online");
 });
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log("Server online");
-});
+export default app;
--- src/middlewares/handlerError.ts
+++ src/middlewares/handlerError.ts
@@ -1,4 +1,4 @@
-import { Errback, Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export default async function handlerError(
     error: any,
@@ -6,11 +6,12 @@ export default async function handlerError(
     res: Response,
     next: NextFunction
 ) {
-    if ((error.type = "unprocessableEntity")) return res.sendStatus(422);
-    if ((error.type = "notFound")) return res.sendStatus(404);
-    if ((error.type = "conflict")) return res.sendStatus(409);
-    if ((error.type = "unauthorized")) return res.sendStatus(401);
-
     console.log("Error: ", error);
+
+    if ((error.type === "unprocessableEntity")) return res.sendStatus(422);
+    if ((error.type === "notFound")) return res.sendStatus(404);
+    if ((error.type === "conflict")) return res.sendStatus(409);
+    if ((error.type === "unauthorized")) return res.sendStatus(401);
+
     return res.sendStatus(500);
 }
