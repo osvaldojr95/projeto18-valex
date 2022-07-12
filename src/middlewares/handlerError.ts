de22444850c139e2f2dc8ea481e6434add837ea7
Osvaldo Júnior <osvaldojunior95@gmail.com>
2022-07-12 15:07:52 -0300

    fix: ajustado erros no handlerError

--- src/middlewares/handlerError.ts
+++ src/middlewares/handlerError.ts
@@ -1,11 +1,16 @@
 import { Errback, Request, Response, NextFunction } from "express";
 
 export default async function handlerError(
-    error: Errback,
+    error: any,
     req: Request,
     res: Response,
     next: NextFunction
 ) {
+    if ((error.type = "unprocessableEntity")) return res.sendStatus(422);
+    if ((error.type = "notFound")) return res.sendStatus(404);
+    if ((error.type = "conflict")) return res.sendStatus(409);
+    if ((error.type = "unauthorized")) return res.sendStatus(401);
+
     console.log("Error: ", error);
     return res.sendStatus(500);
 }
